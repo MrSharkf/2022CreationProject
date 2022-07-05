df91598c6054256975fcb4db580a4f6a3623f4de
MrSharkf <mr.sharkf@foxmail.com>
2022-07-05 23:17:40 +0800

    startup

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
-//app.js
-const AV = require('./libs/av-core-min');
-const adapters = require('./libs/leancloud-adapters-weapp.js');
+import { CLOUD_ENV_ID } from './config'
 
-AV.setAdapters(adapters);
-AV.init({
-  appId: 'mRVzcVTHV7lbeph0ErsPjOql-gzGzoHsz',
-  appKey: '06f6PwieJU4LdJUtRqiALAbu',
-  // 请将 xxx.example.com 替换为你的应用绑定的自定义 API 域名
-  serverURLs: "https://mrvzcvth.lc-cn-n1-shared.com",
-});
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch() {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+        env: CLOUD_ENV_ID
+      })
+    }
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
+    this.data = {
+      timerId: -1,
+      timerState: TimerState.NONE,
+      goalId: '',
+      goalTitle: '',
+      duration: 0,
+      beginDate: 0,
+      pauseDate: 0,
+      pauseDuration: 0
+    }
   }
 })
--- pages/message/message.js
+++ pages/message/message.js
@@ -5,90 +5,62 @@ Page({
    * 页面的初始数据
    */
   data: {
-    message:'',
-    showVideo:false,
-    sourceOfVideo:'https://cdm-oss.oss-cn-zhangjiakou.aliyuncs.com/motionBankForOldPeople/%E8%80%81%E5%B9%B4%E4%BA%BA%E7%83%AD%E8%BA%AB%E8%BF%90%E5%8A%A8/MOTION_WARMUP_1.m4v'
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.setData({
-      message: options.message,
-    })
+  onLoad(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-    this.videoContext = wx.createVideoContext('myVideo')
+  onReady() {
 
   },
-
-  showVideo:function(){
-
-    this.setData({
-      showVideo:true
-    })
-
-
-  },
-
-  // button 播放
-  btnVideoPlay: function () {
-
-    this.videoContext.play()
-
-  },  
-
-  btnVideoPause: function () {
-
-    this.videoContext.pause()
-
-  }, 
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
   }
 })
