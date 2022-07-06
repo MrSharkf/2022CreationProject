7cdea8f97428548bdcf22f45a9b8fb631af0ee91
wpfwpfwpfwpf <108324193+wpfwpfwpfwpf@users.noreply.github.com>
2022-07-06 15:01:53 +0800

    试试怎么传

---
+++ 新建文件夹/index.js
@@ -0,0 +1,30 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    
+  },
+  goToPage1:function(){
+    wx.navigateTo({
+      url: '',
+    })
+  },
+ goToPage2:function(){
+    wx.navigateTo({
+      url: '',
+    })
+  },
+    goToPage3:function(){
+      wx.navigateTo({
+        url: '',
+      })
+  },
+  goToPage4:function(){
+    wx.navigateTo({
+      url: '',
+    })
+  }
+  
+})
